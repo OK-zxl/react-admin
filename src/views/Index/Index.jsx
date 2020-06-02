6316158056937dec32c602e1d2c4a9dbf4b039e5
xzl <xzl865638012@163.com>
2020-06-03 02:27:03 +0800

    增加类别图标

--- src/views/Index/Index.jsx
+++ src/views/Index/Index.jsx
@@ -1,7 +1,11 @@
-import React, { Component } from 'react'
-import { Layout, Row, Col, Icon, Card } from 'antd'
-import screenfull from 'screenfull'
-import '@/style/view-style/index.scss'
+import React, { Component } from "react";
+import { Layout, Row, Col, Card } from "antd";
+import screenfull from "screenfull";
+import "@/style/view-style/index.scss";
+import vue from "../../assets/images/Vue.png";
+import react from "../../assets/images/react.png";
+import wechat from "../../assets/images/wechat.png";
+import app from "../../assets/images/app.png";
 
 // import BarEcharts from './bar.jsx'
 // import PieEcharts from './pie.jsx'
@@ -10,85 +14,85 @@ import '@/style/view-style/index.scss'
 // import PictorialBarEcharts from './pictorialBar.jsx'
 
 class Index extends Component {
-    fullToggle = () => {
-        if (screenfull.isEnabled) {
-            screenfull.request(document.getElementById('bar'))
-        }
+  fullToggle = () => {
+    if (screenfull.isEnabled) {
+      screenfull.request(document.getElementById("bar"));
     }
-    render() {
-        return (
-            <Layout className='index animated fadeIn'>
-                <Row gutter={24} className='index-header'>
-                    <Col span={6}>
-                        <div className='base-style vuejs'>
-                            <Icon type='wechat' className='icon-style' />
-                            <div>
-                                <span>999</span>
-                                <div>VUE</div>
-                            </div>
-                        </div>
-                    </Col>
-                    <Col span={6}>
-                        <div className='base-style reactjs'>
-                            <Icon type='qq' className='icon-style' />
-                            <div>
-                                <span>366</span>
-                                <div>REACT</div>
-                            </div>
-                        </div>
-                    </Col>
-                    <Col span={6}>
-                        <div className='base-style wechat'>
-                            <Icon type='dingding' className='icon-style' />
-                            <div>
-                                <span>666</span>
-                                <div>小程序</div>
-                            </div>
-                        </div>
-                    </Col>
-                    <Col span={6}>
-                        <div className='base-style weibo'>
-                            <Icon type='weibo' className='icon-style' />
-                            <div>
-                                <span>689</span>
-                                <div>Native</div>
-                            </div>
-                        </div>
-                    </Col>
-                </Row>
-                <Row gutter={24}>
-                    <Col span={6} >
-                      <Card title="往期热门文章排行（前十）" >
-                        <p>Card content</p>
-                        <p>Card content</p>
-                        <p>Card content</p>
-                      </Card>
-                    </Col>
-                    <Col span={6}>
-                      <Card title="往期热门文章排行（前十）">
-                        <p>Card content</p>
-                        <p>Card content</p>
-                        <p>Card content</p>
-                      </Card>
-                    </Col>
-                    <Col span={6}>
-                      <Card title="往期热门文章排行（前十）" >
-                        <p>Card content</p>
-                        <p>Card content</p>
-                        <p>Card content</p>
-                      </Card>
-                    </Col>
-                    <Col span={6}>
-                      <Card title="往期热门文章排行（前十）" >
-                        <p>Card content</p>
-                        <p>Card content</p>
-                        <p>Card content</p>
-                      </Card>
-                    </Col>
-                </Row>
-            </Layout>
-        )
-    }
+  };
+  render() {
+    return (
+      <Layout className="index animated fadeIn">
+        <Row gutter={24} className="index-header">
+          <Col span={6}>
+            <div className="base-style vuejs">
+              <img src={vue} alt="" className="icon-style"></img>
+              <div>
+                <span>999</span>
+                <div>VUE</div>
+              </div>
+            </div>
+          </Col>
+          <Col span={6}>
+            <div className="base-style reactjs">
+              <img src={react} alt="" className="icon-style"></img>
+              <div>
+                <span>366</span>
+                <div>REACT</div>
+              </div>
+            </div>
+          </Col>
+          <Col span={6}>
+            <div className="base-style wechat">
+              <img src={wechat} alt="" className="icon-style"></img>
+              <div>
+                <span>666</span>
+                <div>小程序</div>
+              </div>
+            </div>
+          </Col>
+          <Col span={6}>
+            <div className="base-style native">
+              <img src={app} alt="" className="icon-style"></img>
+              <div>
+                <span>689</span>
+                <div>Native</div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+        <Row gutter={24}>
+          <Col span={6}>
+            <Card title="往期热门文章排行（前十）">
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card title="往期热门文章排行（前十）">
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card title="往期热门文章排行（前十）">
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card title="往期热门文章排行（前十）">
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Card>
+          </Col>
+        </Row>
+      </Layout>
+    );
+  }
 }
 
-export default Index
+export default Index;
